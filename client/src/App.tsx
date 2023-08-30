4858b155a17b4f332aea6537cce7be39180f003d
GanievaSabina202 <ganievasabina202@gmail.com>
2023-08-30 23:25:11 +0400

    First commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,52 +1,17 @@
-import { Button } from "./components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./components/ui/dialog";
-import { Input } from "./components/ui/input";
-import { Label } from "./components/ui/label";
+import React from 'react';
+import AddTask from './features/addTask';
+import Tasks from './features/tasks/Tasks';
 
-function App() {
+
+const App = () => {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Add task</Button>
-      </DialogTrigger>
-
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  );
+    <div className="container mx-auto mt-2">
+      <AddTask />
+      <div className="grid grid-cols-3 gap-4 mt-2">
+        <Tasks />
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
