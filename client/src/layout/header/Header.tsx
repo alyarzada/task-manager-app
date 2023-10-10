65d1834ec4adbb386ebaa2673b0b409645042824
GanievaSabina202 <ganievasabina202@gmail.com>
2023-10-10 12:04:19 +0400

    all task & my task add

--- client/src/layout/header/Header.tsx
+++ client/src/layout/header/Header.tsx
@@ -1,7 +1,31 @@
+import { Button } from "../../components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../../components/ui/avatar";
-const Header = () => {
+import { Api } from "../../api/Api";
+const Header = ({ setData }) => {
+
+  const AllTasks = async () => {
+    try {
+      const response = await Api().get('/api/tasks/all');
+      setData(response.data.tasks)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  const MyTasks = async () => {
+    try {
+      const response = await Api().get('/api/tasks');
+      setData(response.data.tasks)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="pt-6 pb-6 flex justify-end">
+    <div className="pt-6 pb-6 flex justify-between">
+      <div>
+        <Button className="mr-5" onClick={AllTasks}>All Tasks</Button>
+        <Button onClick={MyTasks}>My Tasks</Button>
+      </div>
       <div className="flex justify-between items-center">
         <p className="font-medium mr-3">Filankes Filankesov</p>
         <Avatar>
