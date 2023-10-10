ac533e9a23e9fe0b1a7c14de4b6e93415e584177
GanievaSabina202 <ganievasabina202@gmail.com>
2023-10-10 12:48:21 +0400

    change component

--- client/src/layout/header/Header.tsx
+++ client/src/layout/header/Header.tsx
@@ -1,20 +1,29 @@
+import { useState } from "react";
 import { Button } from "../../components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "../../components/ui/avatar";
 import { Api } from "../../api/Api";
+
 const Header = ({ setData }) => {
+  const [showMyTasks, setShowMyTasks] = useState(true)
+  const [showAllTasks, setShowAllTasks] = useState(false);
 
   const AllTasks = async () => {
     try {
       const response = await Api().get('/api/tasks/all');
-      setData(response.data.tasks)
+      setData(response.data.tasks);
+      setShowMyTasks(false)
+      setShowAllTasks(true)
     } catch (error) {
       console.log(error);
     }
   }
+
   const MyTasks = async () => {
     try {
       const response = await Api().get('/api/tasks');
       setData(response.data.tasks)
+      setShowMyTasks(true)
+      setShowAllTasks(false)
     } catch (error) {
       console.log(error);
     }
@@ -23,8 +32,8 @@ const Header = ({ setData }) => {
   return (
     <div className="pt-6 pb-6 flex justify-between">
       <div>
-        <Button className="mr-5" onClick={AllTasks}>All Tasks</Button>
-        <Button onClick={MyTasks}>My Tasks</Button>
+        <Button className="mr-5" onClick={AllTasks} disabled={showAllTasks}>All Tasks</Button>
+        <Button onClick={MyTasks} disabled={showMyTasks}>My Tasks</Button>
       </div>
       <div className="flex justify-between items-center">
         <p className="font-medium mr-3">Filankes Filankesov</p>
